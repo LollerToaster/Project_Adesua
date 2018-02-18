58924d22631cc42e0146bb5e6b18e612b5103963
Adrian Angub <adrianangub@gmail.com>
2018-02-18 19:25:29 +0800

    // Test //

---
+++ app/src/main/java/com/project/adrianangub/project_adesua/LoginPage.java
@@ -0,0 +1,29 @@
+package com.project.adrianangub.project_adesua;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+
+
+        //DANGER
+        Button btn = (Button)findViewById(R.id.loginButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginPage.this, mainMenu.class));
+            }
+        });
+        //DANGER
+
+    }
+}
---
+++ app/src/main/java/com/project/adrianangub/project_adesua/bookInfoPage.java
@@ -0,0 +1,60 @@
+package com.project.adrianangub.project_adesua;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class bookInfoPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_info_page);
+
+        // Toolbar Related =========================================================================
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        if (getSupportActionBar() != null) {
+            myToolbar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#2B3642")));
+            //getSupportActionBar().setTitle("Test [Proj Adesua]");
+            getSupportActionBar().setTitle(Html.fromHtml("<font color='#25B18A'>[ Project Adesua ]</font>"));
+
+        }
+        // Toolbar Related =========================================================================
+    }
+
+    // Settings Button Related =====================================================================
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the main_menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.appbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.item1:
+                //your action
+                break;
+            case R.id.item2:
+                //your action
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
+    }
+    // Settings Button Related =====================================================================
+
+}
